1fe2c415172f6ba9793cb6690aa691820481c876
Daniel <daniel@becher.com.br>
2022-03-03 15:33:43 -0300

    Ferramenta de REGEX, aula sobre expressões regulares

--- bytebank/index.js
+++ bytebank/index.js
@@ -1,12 +1,21 @@
-import {Cliente} from "./Cliente.js"
-import { Conta } from "./Conta.js";
-import {ContaCorrente} from "./ContaCorrente.js"
-import {ContaPoupanca} from "./ContaPoupanca.js";
+import {Cliente} from "./Cliente.js";
+import {Gerente} from "./funcionario/Gerente.js";
+import {Diretor} from "./funcionario/Diretor.js";
+import {ContaCorrente} from "./Conta/ContaCorrente.js"
+import {ContaPoupanca} from "./Conta/ContaPoupanca.js";
+import {ContaSalario} from "./Conta/ContaSalario.js";
+import {SistemaAutenticacao} from "./SistemaAutenticacao.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
 
-const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-const conta = new Conta(0,cliente1,1001)
+const diretor = new Diretor("Rodrigo",10000,12345678900);
+diretor.cadastrarSenha("123456")
+const gerente = new Gerente("Ricardo",5000,98765432100);
+gerente.cadastrarSenha("123");
 
-console.log(conta);
+const cliente = new Cliente("Lais", 22233344455, "456");
+const gerenteestaLogado = SistemaAutenticacao.login(gerente,"123");
+const diretorestaLogado = SistemaAutenticacao.login(diretor,"123456");
+const clienteestaLogado = SistemaAutenticacao.login(cliente,"456");
+
+console.log(gerenteestaLogado, diretorestaLogado, clienteestaLogado);
